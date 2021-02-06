4e42d30f38481d2eed50df764cbdb5b2412edfd9
atilaoliveirajr <atila.oliveira.jr@gmail.com>
2021-02-06 20:26:58 +0100

    Imported Leaflet config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IssPositionMapComponent } from './components/iss-position-map/iss-position-map.component';
 
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     IssPositionMapComponent,
-    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LeafletModule
   ],
   providers: [],
   bootstrap: [AppComponent]
