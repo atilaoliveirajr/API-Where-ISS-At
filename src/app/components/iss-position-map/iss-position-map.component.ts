aba97c557f73d18d9aef10f12a680916e638cb6b
atilaoliveirajr <atila.oliveira.jr@gmail.com>
2021-02-07 12:41:53 +0100

    Added icon customization

--- src/app/components/iss-position-map/iss-position-map.component.ts
+++ src/app/components/iss-position-map/iss-position-map.component.ts
@@ -8,43 +8,54 @@ import * as L from 'leaflet';
   styleUrls: ['./iss-position-map.component.scss']
 })
 export class IssPositionMapComponent implements OnInit, AfterViewInit {
-  private map: any;
 
-  tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  maxZoom: 19,
-  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  // Set a custom Icon
+  issIcon = L.icon({
+    iconUrl: '../../../assets/img/International_Space_Station.svg.png',
+    iconSize:     [50, 32], // size of the icon
+    shadowSize:   [25, 16], // size of the shadow
+    iconAnchor:   [25, 16], // point of the icon which will correspond to marker's location
   });
 
-  api_url: string = 'https://api.wheretheiss.at/v1/satellites/25544';
-  longitude: string = '';
-  latitude: string = '';
 
-  private initMap(): void {
-    this.map = L.map('map', {
-      center: [ 39.8282, -98.5795 ],
-      zoom: 3
-    });
-  }
+  // Set parameters for the Map (Tile).
+  tileURL: string = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+  attribution: string = '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>';
+  maxZoom: number = 19;
+  tiles = L.tileLayer(this.tileURL, { maxZoom: this.maxZoom, attribution: this.attribution});
+
+  api_url: string = 'https://api.wheretheiss.at/v1/satellites/25544';
+  longitude: number = 0;
+  latitude: number = 0;
 
   constructor() { }
-  
+
+  // Init the map
+  private initMap(): void {
+    this.getIss().then(() => {
+      const map = L.map('map', {
+        center: [ this.latitude, this.longitude ],
+        zoom: 4
+      });
+      this.tiles.addTo(map);
+      L.marker([ this.latitude, this.longitude ], {icon: this.issIcon}).addTo(map);
+    })
+  }
+
+  // Get the info from the WTIA REST API source URL: https://api.wheretheiss.at/v1/satellites/25544
   async getIss() {
     const response = await fetch(this.api_url);
     const data = await response.json();
     const {latitude, longitude} = data;
     this.latitude = latitude;
     this.longitude = longitude;
-    console.log(latitude, longitude)
   }
 
   ngOnInit(): void {
-    this.getIss()
+    L.Icon.Default.imagePath = "../../../assets/img/";
   }
 
   ngAfterViewInit(): void {
-    
     this.initMap();
-    this.tiles.addTo(this.map);
   }
-
 }
